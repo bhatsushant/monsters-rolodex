5568bb7fabb29f72c6339866fa96ad1510df4af5
Sushant <sush1393@gmail.com>
2020-05-20 14:12:45 +0530

    Updates

---
+++ precache-manifest.05618d07e016d41f2279f08682c9e15b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3db3b412c58afc6b28446e1b28b2220",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c3c7811cfca95dc009c5",
+    "url": "/monsters-rolodex/static/css/main.43dd6c04.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c7811cfca95dc009c5",
+    "url": "/monsters-rolodex/static/js/main.20edf1f4.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
